331c4495785d693fbcb711a9d7edcfffe6f4cfbc
nebbieridzemariam <nebbieridze.mariam@gmail.com>
2021-12-23 09:06:22 +0400

    add js

---
+++ apps.js
@@ -0,0 +1,14 @@
+const myName= "Mariam";
+let myAge = 23;
+const myHobby='Watching Movies'
+const amIAdult = true;
+const myNull = null;
+const myUndefined= undefined;
+
+console.log (myName);
+console.log (myAge);
+console.log (myHobby);
+console.log (amIAdult);
+console.log (`My name is: ${myName} my age is: ${myAge} and my hobby is: ${myHobby} and I am an: ${amIAdult}`)
+console.log ( `myNull`, typeof myNull);
+console.log (  `myUndefined`, typeof myUndefined);
